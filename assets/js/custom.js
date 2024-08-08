64099bfe269d8a99c3a9c911cdc4e75378f324d6
Aditya Raj Singh <adityashailendra477@gmail.com>
2024-08-08 18:18:16 +0530

    Nav Bar link attachment

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => {
-    const preloader = document.querySelector('.loader_new');
-    preloader.style.opacity = '0';
-    setTimeout(() => {
-      preloader.style.display = 'none';
-    }, 3000); // Adjust delay as needed
-  });
+  const preloader = document.querySelector('.loader_new');
+  preloader.style.opacity = '0';
+  setTimeout(() => {
+    preloader.style.display = 'none';
+  }, 3000); // Adjust delay as needed
+});
